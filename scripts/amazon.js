0f7cc4ce487c3e2fa3cbd58c6bce2894cb63ddb6
Amazon-Javascripting-Project <hashirkamilbaig2003@gmail.com>
2024-08-27 22:43:21 -0700

    Save data and generate HTML for payment summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,87 +1,81 @@
-import { cart, addToCart} from '../data/cart.js';
-import { products } from '../data/products.js';
-//import { addToCart } from '../data/cart.js';
-import { formatCurrency } from './utils/money.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
-products.forEach((product, index)=>{
-    productsHTML += `
-        <div class="product-container">
-        <div class="product-image-container">
+products.forEach((product) => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
         <img class="product-image"
-            src="${product.image}">
-        </div>
+          src="${product.image}">
+      </div>
 
-        <div class="product-name limit-text-to-2-lines">
+      <div class="product-name limit-text-to-2-lines">
         ${product.name}
-        </div>
+      </div>
 
-        <div class="product-rating-container">
+      <div class="product-rating-container">
         <img class="product-rating-stars"
-            src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-            ${product.rating.count}
+          ${product.rating.count}
         </div>
-        </div>
+      </div>
 
-        <div class="product-price">
+      <div class="product-price">
         $${formatCurrency(product.priceCents)}
-        </div>
+      </div>
 
-        <div class="product-quantity-container">
+      <div class="product-quantity-container">
         <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
-        </div>
+      </div>
 
-        <div class="product-spacer"></div>
+      <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+      <div class="added-to-cart">
         <img src="images/icons/checkmark.png">
         Added
-        </div>
+      </div>
 
-        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
-        </button>
+      </button>
     </div>
-    `;
-    
-})
+  `;
+});
 
-//console.log(productsHTML);
-
-document.querySelector('.js-products-grid')
-    .innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-    let cartQuantity = 0;
+  let cartQuantity = 0;
 
-            cart.forEach((cartItem)=>{
-                cartQuantity += cartItem.quantity;
-            })
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-        button.addEventListener('click', () => {
-            const productId = button.dataset.productId;
-
-           
-            addToCart(productId);
-            updateCartQuantity();
-            
-        })
-});
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
