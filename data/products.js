6e139c8680cfa62586e0adfc84e448d737d55ea7
Amazon-Javascripting-Project <hashirkamilbaig2003@gmail.com>
2024-09-02 08:33:48 -0700

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -65,7 +65,30 @@ const object2 = {
   b: this.a,
 };*/
 
+export let products = [];
 
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', ()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -731,4 +754,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
